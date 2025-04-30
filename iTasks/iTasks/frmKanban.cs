f719c8cf146ae5c92e986c0eb57282c6a8f7cd11
Gugas749 <conradoalexandre00@gmail.com>
2025-04-30 11:36:09 +0100

    upload

---
+++ iTasks/iTasks/frmKanban.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTasks
+{
+    public partial class frmKanban : Form
+    {
+        public frmKanban()
+        {
+            InitializeComponent();
+        }
+
+        private void frmKanban_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
